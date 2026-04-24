d6b5b146e2a04868c68f7009182884f599bc3048
dongkeun0107 <dongkeun0107@gmail.com>
2026-04-24 13:50:22 +0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: "나트랑 가족여행",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
